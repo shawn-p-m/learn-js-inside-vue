b04caccb989964de924dced76b1dc3aca373ac9c
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2021-01-26 08:40:50 -0800

    Project setup correctly

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <button @click="arrowFunction">Arrow function</button>
-
+    <v-app>
+      <img
+        width="200"
+        class="mx-auto mt-5"
+        alt="Vue logo"
+        src="./assets/logo.png"
+      />
+      <v-btn
+        :color="buttons[0].buttonColor"
+        width="200"
+        class="mx-auto mt-5"
+        @click="arrow"
+      >
+        Arrow Function
+      </v-btn>
+    </v-app>
   </div>
 </template>
 
 <script>
-import {arrowFunction} from "@/mixins/specifics";
+import { arrowFunction } from "@/mixins/specifics";
 
 export default {
-  data: function() {
-return {
-    arrowFunction: arrowFunction
-  };
-},
-  name: 'App',
+  name: "App",
 
-
-}
+  data: () => {
+    return {
+      buttons: [
+        { complete: false, buttonColor: "primary" },
+        { complete: false, buttonColor: "primary" },
+      ],
+    };
+  },
+  methods: {
+    arrow: () => {
+      if (arrowFunction() === 3) {
+        alert("Bingo");
+        this.buttons[0].buttonColor = "success";
+      } else {
+        alert("Try Again");
+        this.buttons[0].buttonColor = "failure";
+      }
+    },
+  },
+};
 </script>
-
-<style lang="scss">
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-</style>
