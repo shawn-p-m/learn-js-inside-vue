aa4d9140f37ecd5ae88c1f467d4cbdf6f50f2481
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2021-01-26 19:34:06 -0800

    button turns green or blue if function returns correct value

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,10 @@
         src="./assets/logo.png"
       />
       <v-btn
-        :color="buttons[0].buttonColor"
+        :color="buttonColor"
         width="200"
         class="mx-auto mt-5"
-        @click="arrow"
+        @click="buttonColor = arrow()"
       >
         Arrow Function
       </v-btn>
@@ -26,22 +26,11 @@ export default {
   name: "App",
 
   data: () => {
-    return {
-      buttons: [
-        { complete: false, buttonColor: "primary" },
-        { complete: false, buttonColor: "primary" },
-      ],
-    };
+    return { clicked: false, buttonColor: "primary" };
   },
   methods: {
     arrow: () => {
-      if (arrowFunction() === 3) {
-        alert("Bingo");
-        this.buttons[0].buttonColor = "success";
-      } else {
-        alert("Try Again");
-        this.buttons[0].buttonColor = "failure";
-      }
+      return arrowFunction() === 3 ? "success" : "error";
     },
   },
 };
