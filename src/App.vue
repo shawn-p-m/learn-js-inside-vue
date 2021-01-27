367abd272741e981d9160d76a6d0456b147451e7
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2021-01-27 14:02:36 -0800

    buttons have state management with Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,87 @@
 <template>
   <div id="app">
     <v-app>
-      <img
-        width="200"
-        class="mx-auto mt-5"
-        alt="Vue logo"
-        src="./assets/logo.png"
-      />
-      <v-btn
-        :color="buttonColor"
-        width="200"
-        class="mx-auto mt-5"
-        @click="buttonColor = arrow()"
-      >
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <v-btn :color="arrowButtonColor" @click="arrowButtonColor = arrow()">
         Arrow Function
+      </v-btn>
+      <v-btn :color="mapButtonColor" @click="mapButtonColor = map()">
+        Map Function
       </v-btn>
     </v-app>
   </div>
 </template>
 
 <script>
-import { arrowFunction } from "@/mixins/specifics";
+import { arrowFunction, numsWithOddOrEven } from "@/mixins/specifics";
+
+const giveSuccessOrError = (condition) => {
+  if (condition) {
+    return "success";
+  } else {
+    return "error";
+  }
+};
 
 export default {
   name: "App",
+  created: function () {
+    if (
+      this.arrowButtonColor === "error" ||
+      this.arrowButtonColor === "success"
+    )
+      this.arrow();
 
+    if (this.mapButtonColor === "error" || this.mapButtonColor === "success")
+      this.map();
+    this.arrowButtonColor = this.$store.state.arrowButtonColor;
+    this.mapButtonColor = this.$store.state.mapButtonColor;
+  },
   data: () => {
-    return { clicked: false, buttonColor: "primary" };
+    return {
+      arrowButtonColor: "",
+      mapButtonColor: "",
+    };
   },
   methods: {
-    arrow: () => {
-      return arrowFunction() === 3 ? "success" : "error";
+    arrow: function () {
+      this.$store.commit(
+        "saveArrowButtonColor",
+        giveSuccessOrError(arrowFunction() === 3)
+      );
+
+      return this.$store.state.arrowButtonColor;
+    },
+    map: function () {
+      this.$store.commit(
+        "saveMapButtonColor",
+        giveSuccessOrError(
+          JSON.stringify(numsWithOddOrEven) ===
+            JSON.stringify([
+              "12: is even",
+              "13: is odd",
+              "16: is even",
+              "17: is odd",
+              "8: is even",
+              "32: is even",
+              "33: is odd",
+            ])
+        )
+      );
+
+      return this.$store.state.mapButtonColor;
     },
   },
 };
 </script>
+
+<style lang="scss">
+img {
+  width: 200px;
+  margin: 5px auto;
+}
+.v-btn {
+  width: 200px;
+  margin: 10px auto;
+}
+</style>
